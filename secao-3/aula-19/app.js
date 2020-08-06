679a2cd76cf571a1f8e81edfbddc06c34b3db2b3
André Rodrigues <andre_santos_t@hotmail.com>
2020-08-06 00:13:43 -0400

    Seção 3 - Aula 19 - Assign Variable Value Later

---
+++ secao-3/aula-19/app.js
@@ -0,0 +1,7 @@
+let name = 'John shrimp taco VI';
+let address, zip, state;
+address = '101 main street';
+zip = ['60612', 60612];
+state = 'CA';
+console.log(address, zip, state);
+console.log(zip);
