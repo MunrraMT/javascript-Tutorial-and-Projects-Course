68d08f950385d6e891c506b60ce49cf34052341e
Andre Rodrigues Melgaco dos Santos <dev-asantos@outlook.com>
2020-08-17 17:34:35 -0400

    Seção 3 - Aula 27 - Data Types

---
+++ secao-3/aula-27/app.js
@@ -0,0 +1,29 @@
+// String
+const text = 'some text';
+// Number
+const numero = 29;
+// Boolean
+let boo1 = true;
+let boo2 = false;
+// Null
+const nuu = null;
+// Undefined
+let name;
+// Symbol(ES6)
+
+console.log(typeof text);
+console.log(typeof 'text');
+
+console.log(typeof numero);
+console.log(typeof 29);
+
+console.log(typeof boo1);
+console.log(typeof true);
+console.log(typeof boo2);
+console.log(typeof false);
+
+console.log(nuu);
+console.log(null);
+
+console.log(name);
+console.log(undefined);
