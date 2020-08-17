06ab3b33df6a5cf07a78b3366fe00e6639e9e8b5
Andre Rodrigues Melgaco dos Santos <dev-asantos@outlook.com>
2020-08-17 14:50:05 -0400

    Seção 3 - Aula 21 - const, let, var

---
+++ secao-3/aula-21/app.js
@@ -0,0 +1,9 @@
+var value = 'some value';
+value = 'some other value';
+let name = 'john';
+name = 'peter';
+const lastname = 'jordan';
+
+console.log(value);
+console.log(name);
+console.log(lastname);
