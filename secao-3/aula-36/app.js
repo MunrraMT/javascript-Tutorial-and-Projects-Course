3da55279252f94ef70a9d531011131636672a3d9
Andre Rodrigues Melgaco dos Santos <dev-asantos@outlook.com>
2020-08-20 22:36:00 -0400

    Seção 3 - Aula 36 - Equality

---
+++ secao-3/aula-36/app.js
@@ -0,0 +1,14 @@
+// Conditional Statements
+// Comparison Operators
+// >, <, >=, <=, ==, ===, !=, !==
+// == Check value
+// === Check value and type
+
+const num1 = 6;
+const num2 = '6';
+
+const value = num1 != num2;
+const value2 = num1 !== num2;
+
+console.log(value);
+console.log(value2);
