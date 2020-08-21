4395f39a7b49631323fba06862fab64f1bd16af7
Andre Rodrigues Melgaco dos Santos <dev-asantos@outlook.com>
2020-08-20 22:32:36 -0400

    Seção 3 - Aula 35 - Conditional Statements Continued

---
+++ secao-3/aula-35/app.js
@@ -0,0 +1,22 @@
+// Conditional Statements
+// Comparison Operators
+// >, <, >=, <=, ==, ===, !=, !==
+// else if and !
+
+const num1 = 6;
+const num2 = 6;
+const result = num1 >= num2;
+
+const value = false;
+
+if (!value) {
+	console.log('value is false');
+}
+
+// if (num1 > num2) {
+// 	console.log('first numver is bigger than second');
+// } else if (result) {
+// 	console.log('first number equal to a second');
+// } else {
+// 	console.log('second number is bigger than first');
+// }
