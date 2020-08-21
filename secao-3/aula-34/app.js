5fdce614f54be09c52f1e77c45a44556d623e21d
Andre Rodrigues Melgaco dos Santos <dev-asantos@outlook.com>
2020-08-20 22:24:13 -0400

    Seção 3 - Aula 34 - Conditional Statements - Basics

---
+++ secao-3/aula-34/app.js
@@ -0,0 +1,11 @@
+// Conditional Statement
+
+const value = 2 > 1;
+// console.log(typeof value);
+const value2 = 1 > 2;
+
+if (value2) {
+	console.log('hello world');
+} else {
+	console.log('hello people');
+}
