13453db215ee46d57293a0628e17fa5be0bf9b0b
Andre Rodrigues Melgaco dos Santos <dev-asantos@outlook.com>
2020-08-20 23:15:01 -0400

    Seção 3 - Aula 41 - for loop

---
+++ secao-3/aula-41/app.js
@@ -0,0 +1,11 @@
+// Loops
+// for loop
+
+// let i;
+// for (i = 0; i <= 10; i++) {
+// 	console.log(`and the number is: ${i}`);
+// }
+
+for (let number = 11; number >= 0; number--) {
+	console.log(`and the number is: ${number}`);
+}
