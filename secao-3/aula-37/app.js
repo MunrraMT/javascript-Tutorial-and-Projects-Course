dd558703c43bcf3917c265f7f55a8e227c7d1cb1
Andre Rodrigues Melgaco dos Santos <dev-asantos@outlook.com>
2020-08-20 22:39:46 -0400

    Seção 3 - Aula 37 - Logical Operators

---
+++ secao-3/aula-37/app.js
@@ -0,0 +1,11 @@
+// Logical Operators
+// (|| - OR), (&& - AND), !
+
+const name = 'peter';
+const age = 24;
+
+if (name !== 'bob' && age === 24) {
+	console.log('hello there user');
+} else {
+	console.log('wrong values');
+}
