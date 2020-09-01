df9c761f9b032b1841d3837223b7a9961b14f1c3
Andre Rodrigues Melgaco dos Santos <dev-asantos@outlook.com>
2020-09-01 13:43:40 -0400

    Seção 4 - Aula 43 - String Properties and Methods - Fazenndo

---
+++ secao-4/aula-43/app.js
@@ -0,0 +1,13 @@
+let text = 'Peter Jordan';
+
+// const person = {
+// 	name: 'peter',
+// 	greeting() {
+// 		console.log("Hey, I'm Peter");
+// 	},
+// };
+
+// console.log(person);
+// console.log(person.name);
+
+// person.greeting();
