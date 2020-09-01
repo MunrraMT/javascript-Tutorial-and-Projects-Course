32915f3dbfc586d15444cadf68dfeab5c59e915f
André Rodrigues <andre_santos_t@hotmail.com>
2020-09-01 14:42:01 -0400

    Seção 4 - Aula 43 - String Properties and Methods - Completo

--- secao-4/aula-43/app.js
+++ secao-4/aula-43/app.js
@@ -1,4 +1,34 @@
-let text = 'Peter Jordan';
+let text = ' Peter Jordan';
+
+let result = text.length;
+console.log(result);
+
+console.log(text.length);
+
+console.log(text.toLowerCase());
+console.log(text.toUpperCase());
+
+console.log(text.charAt(1));
+console.log(text.charAt(7));
+
+console.log(text.charAt(text.length - 1));
+
+console.log(text.indexOf('e'));
+console.log(text.lastIndexOf('e'));
+
+console.log(text);
+console.log(text.trim());
+
+console.log(text.startsWith(' P'));
+console.log(text.startsWith('P'));
+console.log(text.trim().toLowerCase().startsWith('peter'));
+
+console.log(text.includes('eter'));
+
+console.log(text.slice(0, 6));
+console.log(text.slice(-6));
+
+// console.log(text.);
 
 // const person = {
 // 	name: 'peter',
