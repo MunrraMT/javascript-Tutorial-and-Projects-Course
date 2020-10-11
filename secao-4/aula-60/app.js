875ff6356196aa0a7c349af46f9958a87b3c571b
André Rodrigues <andre_santos_t@hotmail.com>
2020-10-11 17:13:27 -0400

    Seção 4 - Aula 60

---
+++ secao-4/aula-60/app.js
@@ -0,0 +1,25 @@
+// FIND
+// ES5 - ARRAY.FIND
+var inventoryA = [
+	{ name: 'apples', quantity: 2 },
+	{ name: 'bananas', quantity: 0 },
+	{ name: 'cherries', quantity: 5 },
+];
+
+function isCherries(fruit) {
+	return fruit.name === 'cherries';
+}
+
+console.log(inventoryA.find(isCherries)); // { name: 'cherries', quantity: 5 }
+
+// ES6 - ARROW FUNCTION - ARRAY.FIND
+const inventoryB = [
+	{ name: 'apples', quantity: 2 },
+	{ name: 'bananas', quantity: 0 },
+	{ name: 'cherries', quantity: 5 },
+	{ name: 'cherries', quantity: 20 },
+];
+
+const result = inventoryB.find((fruit) => fruit.name === 'cherries');
+
+console.log(result); // { name: 'cherries', quantity: 5 }
