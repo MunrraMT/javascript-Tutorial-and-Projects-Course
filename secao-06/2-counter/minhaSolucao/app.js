01bf01c7cc5b22d5a5f61e0ea405f3aada5db85d
André Rodrigues <andre_santos_t@hotmail.com>
2020-10-13 15:40:25 -0400

    Seção 6 - Aula 102

---
+++ secao-06/2-counter/minhaSolucao/app.js
@@ -0,0 +1,19 @@
+const contador = document.querySelector('#value');
+contador.textContent = 0;
+
+document.addEventListener('click', (e) => {
+	const target = e.target;
+	const clicked = target.textContent;
+
+	if (target.tagName === 'BUTTON') {
+		e.preventDefault();
+		clicked === 'increase' && contador.textContent++;
+		clicked === 'reset' && (contador.textContent = 0);
+		clicked === 'decrease' && contador.textContent--;
+	}
+
+	contador.textContent < 0 && (contador.style.color = 'red');
+	contador.textContent > 0 && (contador.style.color = 'green');
+	Number(contador.textContent) === 0 &&
+		(contador.style.color = 'var(--clr-grey-1)');
+});
